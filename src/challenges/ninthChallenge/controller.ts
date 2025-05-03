b569f2177ccffbe9a84238ef857e9a2d68793d1f
Chmod351 <yamil.t.351@proton.me>
2025-05-03 17:52:40 -0300

    simplify error handling

--- src/challenges/ninthChallenge/controller.ts
+++ src/challenges/ninthChallenge/controller.ts
@@ -1,23 +1,24 @@
 import { liqVol, vapVol } from './service.ts';
 import type { Request, Response } from 'express';
 
-async function solveNinthChallenge(req: Request, res: Response) {
+function solveNinthChallenge(req: Request, res: Response) {
   const { pressure } = req.query;
 
   if (!pressure) {
-    res.status(400).json('missing query parameters');
+    throw new Error('missing query parameters');
   }
   const p = Number(pressure);
   if (isNaN(p) || p < 0.05 || p > 10) {
-    return res.status(400).json('invalid pressure value');
+    throw new Error('invalid pressure value');
   }
 
   const aproxLiqVol = liqVol(p);
   const aproxVapVol = vapVol(p);
 
   res.json({
+    status: 'success',
     specific_volume_liquid: aproxLiqVol,
-    specific_volume_vapor: Number(aproxVapVol.toFixed(4)), // redondeamos el valor
+    specific_volume_vapor: Number(aproxVapVol.toFixed(4)),
   });
 }
 export default solveNinthChallenge;
